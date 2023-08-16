02682145900f9c9b72ad91fe80ac2d29c6308a4d
Behnam-am <56001519+Behnam-am@users.noreply.github.com>
2023-08-17 00:02:50 +0330

    error handling

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
+from typing import Optional
+
 import fastapi
 import uvicorn
-
 
 api = fastapi.FastAPI()
 
 
 @api.get("/api/calc")
-def calculate():
-    value = 2 + 2
+def calculate(x: int, y: int, z: Optional[int] = None):
+    if z == 0:
+        return fastapi.responses.JSONResponse(content={"error": "Error: z cannot be zero."},
+                                              status_code=400)
+        # return fastapi.Response(content='{"error": "Error: z cannot be zero."}',
+        #                         media_type="application/json",
+        #                         status_code=400)
+
+    value = (x + y)
+    if z is not None:
+        value /= z
     return {
+        "x": x,
+        "y": y,
+        "z": z,
         "value": value
     }
 
